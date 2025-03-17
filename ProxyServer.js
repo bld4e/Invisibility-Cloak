251da9093823dd6f4a64a120e95a23df51b724a8
Bader <105579892+bld4e@users.noreply.github.com>
2025-03-17 10:07:13 +0300

    Update ProxyServer.js

--- ProxyServer.js
+++ ProxyServer.js
@@ -1,1 +1,13 @@
-
+function browse() {
+    const urlInput = document.getElementById('urlInput').value;
+    console.log(`URL Entered: ${urlInput}`);
+    
+    if (urlInput) {
+        const proxyUrl = `https://cors-anywhere.herokuapp.com/${urlInput}`;
+        console.log(`Proxy URL: ${proxyUrl}`);
+        document.getElementById('proxyFrame').src = proxyUrl;
+        console.log('Iframe source updated.');
+    } else {
+        alert('Please enter a URL');
+    }
+}
